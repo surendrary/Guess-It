10e778b5ca7869e60220b573ca533d5e2eb62b26
Pranjal Jain <Pranjal Jain>
2016-10-07 16:07:07 -0700

    Resolved the issues in text box, only numbers can be entered now

--- StringInputBox.java
+++ StringInputBox.java
@@ -31,26 +31,26 @@ public class StringInputBox extends Actor
         MouseInfo mouse = Greenfoot.getMouseInfo();
         
        if (key == null) return;
-        if ("enter".equals(key) && text.length() > 0)
-        {
-            text = "";
+       try{
+            if ("backspace".equals(key) && text.length() > 0) text = text.substring(0, text.length() - 1);
+            if ("escape".equals(key)) key = "";
+            if ("space".equals(key)) key = "";
+            if ("enter".equals(key)) key = "";    
+            else if(key.length() == 1){
+                inputNumber = Integer.parseInt(key);
+            }
+            
+            if (key.length() == 1 && text.length() < NAME_INPUT_LENGTH ){
+                            if( this.getX()-150 < mouse.getX() &&  mouse.getX() < this.getX()+150 && getWorld().getHeight()/2 > mouse.getY() && mouse.getY()> this.getY() -60)
+                               text += key;                        
+            }    
             updateImage();
-            return;
-        }
-        if ("backspace".equals(key) && text.length() > 0) text = text.substring(0, text.length() - 1);
-        if ("escape".equals(key)) text = "";
-        if ("space".equals(key)) key = " ";
-        if (key.length() == 1 && text.length() < NAME_INPUT_LENGTH ){
-                        if( this.getX()-150 < mouse.getX() &&  mouse.getX() < this.getX()+150 && getWorld().getHeight()/2 > mouse.getY() && mouse.getY()> this.getY() -60)
-                           text += key;                        
-        }    
-        updateImage();
-        try{
             inputNumber = Integer.parseInt(text);
             getWorldOfType(GamePlayWorld.class).enteredNumber = inputNumber;
         }
         catch(NumberFormatException nfException){
                JOptionPane.showMessageDialog(null, "Only integer input is allowed!");
         } 
+
     }
 }

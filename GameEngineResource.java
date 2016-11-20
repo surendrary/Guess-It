84cbd07128060c701dd9a60f6440e89f64e7e8c0
Sucheta Mandal <sucheta.gcetts@gmail.com>
2016-11-19 20:57:47 -0800

    Added webservice and deployement script for game engine resource and database resource

--- GameEngineResource.java
+++ GameEngineResource.java
@@ -1,22 +1,28 @@
+package com;
 import java.util.Random;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.restlet.ext.json.JsonRepresentation;
-import org.restlet.resource.Get;
-import org.restlet.resource.ServerResource;
 
 /**
  * Resource which has only one representation.
  */
-public class GameEngineResource extends ServerResource {
+@Path("/guessit")
+public class GameEngineResource {
 
-	@Get
-	public JsonRepresentation represent() {
+	@GET
+	@Produces("application/json")
+	public Response guessNumber() throws JSONException {
 		int computerGuess = new Random().nextInt(100) + 2;
 		System.out.println("Guessed Number :: " + computerGuess);
 		JSONObject guess = new JSONObject();
 		guess.put("guessedNumber", computerGuess);
-		return new JsonRepresentation(guess);
+		return Response.status(200).entity(guess.toString()).build();
 	}
 
 }
--- GuessDatabaseResource.java
+++ GuessDatabaseResource.java
@@ -1,15 +1,22 @@
+package com;
+
 import java.io.IOException;
 import java.util.UUID;
 
-import org.json.JSONArray;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.restlet.data.Form;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
-import org.restlet.resource.Get;
-import org.restlet.resource.Post;
 import org.restlet.resource.Put;
-import org.restlet.resource.ServerResource;
 
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DB;
@@ -19,95 +26,98 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
-public class GuessDatabaseResource extends ServerResource {
+@Path("/guessit/database")
+public class GuessDatabaseResource {
 
-    @Get
-    public JsonRepresentation represent() {
-        String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
-        MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
-        DB db = mongoClient.getDB("guessit");
-        DBObject query = BasicDBObjectBuilder.start().add("_id", 123).get();
-        JSONObject gameObject = new JSONObject();
+	@GET
+	@Produces("application/json")
+	public Response represent() throws JSONException {
+		String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
+		DB db = mongoClient.getDB("guessit");
+		DBObject query = BasicDBObjectBuilder.start().add("_id", 123).get();
+		JSONObject gameObject = new JSONObject();
 
-        DBCollection gameCollection = db.getCollection("gameTable");
-        DBCursor cursor = gameCollection.find(query);
-        DBObject gameDetails = null;
-        if (cursor != null && cursor.hasNext()) {
-            gameDetails = cursor.next();
-            gameObject.put("game", gameDetails);
-        } else {
-            gameObject.put("message", "No game with this id exists. Please check!");
-        }
-        return new JsonRepresentation(gameObject);
-    }
+		DBCollection gameCollection = db.getCollection("gameTable");
+		DBCursor cursor = gameCollection.find(query);
+		DBObject gameDetails = null;
+		if (cursor != null && cursor.hasNext()) {
+			gameDetails = cursor.next();
+			gameObject.put("game", gameDetails);
+		} else {
+			gameObject.put("message", "No game with this id exists. Please check!");
+		}
+		return Response.status(200).entity(gameObject).build();
+	}
 
-    @Post
-    public JsonRepresentation hostGame(Representation entity) throws IOException {
-        Representation result = null; 
-        String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
-        MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
-        DB db = mongoClient.getDB("guessit");
-        DBCollection gameCollection = db.getCollection("gameTable");
-
-        Form userForm = new Form(entity);
-        String host = userForm.getFirstValue("host");
-        String gameLevel = userForm.getFirstValue("level");
-        String gameName = userForm.getFirstValue("gameName");
-        UUID id = UUID.randomUUID();
-
-        DBObject foundGame = findGame(gameName);
-        if (foundGame == null) {
-            BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
-            docBuilder.append("_id", id);
-            docBuilder.append("host", host);
-            docBuilder.append("level", gameLevel);
-            docBuilder.append("gameName", gameName);
-            DBObject gameObject = docBuilder.get();
-            gameCollection.insert(gameObject);
-            mongoClient.close();
-            return new JsonRepresentation(gameObject);
-        } else {
-            return new JsonRepresentation("Already Exist");
-        }
-    }
-
-    @Put
-    public JsonRepresentation joinGame(Representation entity) {
-       String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.TEXT_PLAIN })
+	public String hostGame(Game entity) throws IOException, JSONException {
+		Representation result = null;
+		String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
 		MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
 		DB db = mongoClient.getDB("guessit");
 		DBCollection gameCollection = db.getCollection("gameTable");
 
-		 Form userForm = new Form(entity);
-	     DBObject foundGame = findGame(userForm.getFirstValue("gameId"));
-	    
-	        if (foundGame==null) {
-	        	return new JsonRepresentation("Game Does Not Exists");
-	           /// JSONObject gameObject = new JSONObject(foundGame);
-	            //JSONArray userArray = gameObject.getJSONArray("users");
+		String host = entity.getHost();
+		String gameLevel = entity.getLevel();
+		String gameName = entity.getGameName();
+		UUID id = UUID.randomUUID();
 
-	            //int length = userArray.length();
-	            // userArray[length]serForm.getFirstValue("user");
-	        }
-    return new JsonRepresentation("");
-	       }
+		DBObject foundGame = findGame(gameName);
+		if (foundGame == null) {
+			BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
+			docBuilder.append("_id", id);
+			docBuilder.append("host", host);
+			docBuilder.append("level", gameLevel);
+			docBuilder.append("gameName", gameName);
+			DBObject gameObject = docBuilder.get();
+			gameCollection.insert(gameObject);
+			mongoClient.close();
+			return "Game Hosted";
+		}
+		return "Error! Game already Exists";
+	}
 
-   private DBObject findGame(String gameName) {
-        String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
-        MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
-        DB db = mongoClient.getDB("guessit");
-        DBObject query = BasicDBObjectBuilder.start().add("gameName", gameName).get();
-        JSONObject gameObject = new JSONObject();
+	@Put
+	public JsonRepresentation joinGame(Representation entity) throws JSONException {
 
-        DBCollection gameCollection = db.getCollection("gameTable");
-        DBCursor cursor = gameCollection.find(query);
-        DBObject gameDetails = null;
-        if (cursor != null && cursor.hasNext()) {
-            gameDetails = cursor.next();
-            gameObject.put("game", gameDetails);
-            mongoClient.close();
-        }
-        return gameDetails;
-    }
+		String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
+		DB db = mongoClient.getDB("guessit");
+		DBCollection gameCollection = db.getCollection("gameTable");
+
+		Form userForm = new Form(entity);
+		DBObject foundGame = findGame(userForm.getFirstValue("gameId"));
+
+		if (foundGame == null) {
+			return new JsonRepresentation("Game Does Not Exists");
+			/// JSONObject gameObject = new JSONObject(foundGame);
+			// JSONArray userArray = gameObject.getJSONArray("users");
+
+			// int length = userArray.length();
+			// userArray[length]serForm.getFirstValue("user");
+		}
+		return new JsonRepresentation("");
+	}
+
+	private DBObject findGame(String gameName) throws JSONException {
+		String dbURI = "mongodb://guessitadmin:techadmin@ds151137.mlab.com:51137/guessit";
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
+		DB db = mongoClient.getDB("guessit");
+		DBObject query = BasicDBObjectBuilder.start().add("gameName", gameName).get();
+		JSONObject gameObject = new JSONObject();
+
+		DBCollection gameCollection = db.getCollection("gameTable");
+		DBCursor cursor = gameCollection.find(query);
+		DBObject gameDetails = null;
+		if (cursor != null && cursor.hasNext()) {
+			gameDetails = cursor.next();
+			gameObject.put("game", gameDetails);
+			mongoClient.close();
+		}
+		return gameDetails;
+	}
 
 }
